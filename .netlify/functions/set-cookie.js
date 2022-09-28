104c93d6b7fecef0de75437f20285a828d7dd6c4
athamov <abdulloxathamoff@gmail.com>
2022-09-28 17:55:39 +0500

    universal-cookie added

--- .netlify/functions/set-cookie.js
+++ .netlify/functions/set-cookie.js
@@ -1,12 +1,40 @@
+const cookie = require('cookie')
+
 exports.handler = async (event, context) => {
+  const hour = 3600000
+  const twoWeeks = 14 * 24 * hour
+  const myCookie = cookie.serialize('refreshToken', 'lolHi', {
+    secure: true,
+    httpOnly: true,
+    path: '/',
+    maxAge: twoWeeks,
+  })
+  const redirectUrl = 'https://basecamp-client.netlify.app'
+  // Do redirects via html
+  const html = `
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <noscript>
+        <meta http-equiv="refresh" content="0; url=${redirectUrl}" />
+      </noscript>
+    </body>
+    <script>
+      setTimeout(function() {
+        window.location.href = ${JSON.stringify(redirectUrl)}
+      }, 0)
+    </script>
+  </html>`
+
   return {
-    statusCode: 200,
-    headers: {
-      "Set-Cookie": "gippea-third-party=true",
-      "Cache-Control": "no-cache",
-      "Content-Type": "text/html",
-      "Access-Control-Allow-Origin": "*",
+    'statusCode': 200,
+    'headers': {
+      'Set-Cookie': myCookie,
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'text/html',
     },
-    body: "alert('cookie set!');",
-  };
-};
+    'body': html
+  }
+}
